9f3cdba83334cb51df1c54bf19abe293c07ab300
loretta-arineva <wowshifter@gmail.com>
2021-06-13 18:27:41 +0800

    Added about audiophile markup

--- src/pages/containers/Home.js
+++ src/pages/containers/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Categories from '../components/CategoryComponents/Categories';
 import Hero from '../components/HeroComponents/Hero';
+import AboutAudioPhile from '../components/HomeComponents/AboutAudioPhile';
 
 const Home = () => {
     return (
         <section id="home">
             <Hero></Hero>
-            <Categories></Categories>
+            <Categories />
+            <AboutAudioPhile />
         </section>
     )
 }
