718ef6465e1af10e1bfdadbb79d4c0ff206112a4
loretta-arineva <wowshifter@gmail.com>
2021-06-17 11:28:42 +0100

    Styled about section for tablet

--- src/pages/components/CategoryComponents/ProductCategory.js
+++ src/pages/components/CategoryComponents/ProductCategory.js
@@ -5,7 +5,9 @@ const ProductCategory = ({ children, image, imgAlt, category }) => {
     const url = `/products/${category}`
     return (
         <div className="category__wrapper">
-            <img src={image} alt={imgAlt} className="category-image" />
+            <div className="category-image__wrapper">
+                <img src={image} alt={imgAlt} className="category-image" />
+            </div>
             <h3 className="category-title">{children}</h3>
             <Link to={url}>Shop <span className="category-arrow">&gt;</span></Link>
         </div>
