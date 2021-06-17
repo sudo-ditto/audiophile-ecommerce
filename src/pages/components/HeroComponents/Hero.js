3ae646f1809791b8d6ac09b9140f5d0e17e37d4c
loretta-arineva <wowshifter@gmail.com>
2021-06-17 10:41:56 +0100

    Added styles for larger devices in hero section

--- src/pages/components/HeroComponents/Hero.js
+++ src/pages/components/HeroComponents/Hero.js
@@ -5,11 +5,13 @@ import Button from '../UI/Button';
 const Hero = () => {
     return (
         <section id="hero">
-            <ProductData newProduct btn="orange" type="hero">
-                <h1 id="hero-title">XX99 Mark II HeadphoneS</h1>
-                <p className="product-description">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-            </ProductData>
-            <Button urlLink="/products/headphones/xx99-mark-two-headphones" className="btn product-btn--orange">See product</Button>
+            <div className="data">
+                <ProductData newProduct btn="orange" type="hero">
+                    <h1 id="hero-title">XX99 Mark II HeadphoneS</h1>
+                    <p className="product-description">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                </ProductData>
+                <Button urlLink="/products/headphones/xx99-mark-two-headphones" className="btn product-btn--orange">See product</Button>
+            </div>
         </section>
     )
 }
